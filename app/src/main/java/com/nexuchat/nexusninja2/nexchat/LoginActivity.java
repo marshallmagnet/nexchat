3bf40bce4161d52f02deb5ab2f58873678bb1e66
marshallmagnet <aljonnale@nexusbond.com>
2017-06-01 12:10:01 +0800

    working google sign-in without database realtime storage

--- app/src/main/java/com/nexuchat/nexusninja2/nexchat/LoginActivity.java
+++ app/src/main/java/com/nexuchat/nexusninja2/nexchat/LoginActivity.java
@@ -10,15 +10,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity
-{
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient googleApiClient;
+
     private TextView txtSignUp;
     private EditText etMail, etPassword;
     private Button btnLogin, btnFb;
@@ -45,7 +55,17 @@ public class LoginActivity extends AppCompatActivity
                 }
             }
         };
-        //asdjasd
+
+        GoogleSignInOptions googleSignOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignOptions)
+                .build();
+
         etMail = (EditText)findViewById(R.id.etEmail);
         etPassword = (EditText)findViewById(R.id.etPassword);
         txtSignUp = (TextView)findViewById(R.id.txtSignUp);
@@ -57,24 +77,6 @@ public class LoginActivity extends AppCompatActivity
         btnLogin.setOnClickListener(onClick());
         btnFb.setOnClickListener(onClick());
         btnGoogle.setOnClickListener(onClick());
-
-        mAuthlistener = new FirebaseAuth.AuthStateListener()
-        {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
-            {
-                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-
-                if (currentUser != null)
-                {
-                    GoHome();
-                }
-                else if (currentUser == null)
-                {
-
-                }
-            }
-        };
     }
 
     @Override
@@ -95,6 +97,55 @@ public class LoginActivity extends AppCompatActivity
             mAuth.removeAuthStateListener(mAuthlistener);
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct)
+    {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toaster("Authentication failed.");
+                        }
+                    }
+                });
+    }
+
+    private void SignInGoogle()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     private void LoginUser(String email, String password)
     {
@@ -151,9 +202,15 @@ public class LoginActivity extends AppCompatActivity
                 }
                 else if(buttonID == R.id.btnGoogle)
                 {
-
+                    SignInGoogle();
                 }
             }
         };
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
+
     }
 }
