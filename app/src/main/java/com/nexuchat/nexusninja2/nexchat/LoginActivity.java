864edf004735997a170a6b05c2377533e3df6bab
marshallmagnet <aljonnale@nexusbond.com>
2017-06-02 11:33:42 +0800

    update 6/2/17

--- app/src/main/java/com/nexuchat/nexusninja2/nexchat/LoginActivity.java
+++ app/src/main/java/com/nexuchat/nexusninja2/nexchat/LoginActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -23,6 +30,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -35,11 +43,13 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     private TextView txtSignUp;
     private EditText etMail, etPassword;
-    private Button btnLogin, btnFb;
+    private Button btnLogin;
+    private LoginButton loginButtonFb;
     private SignInButton btnGoogle;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthlistener;
     private DatabaseReference databaseReference;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -72,16 +82,44 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignOptions)
                 .build();
 
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.btnFb);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                //Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                //Toaster();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel()
+            {
+                //Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+                //Toaster(error.toString());
+            }
+        });
+
+
         etMail = (EditText)findViewById(R.id.etEmail);
         etPassword = (EditText)findViewById(R.id.etPassword);
         txtSignUp = (TextView)findViewById(R.id.txtSignUp);
         btnLogin = (Button)findViewById(R.id.btnLogin);
-        btnFb = (Button)findViewById(R.id.btnFb);
+        //btnFb = (Button)findViewById(R.id.btnFb);
         btnGoogle = (SignInButton) findViewById(R.id.btnGoogle);
 
         txtSignUp.setOnClickListener(onClick());
         btnLogin.setOnClickListener(onClick());
-        btnFb.setOnClickListener(onClick());
+        //btnFb.setOnClickListener(onClick());
         btnGoogle.setOnClickListener(onClick());
     }
 
@@ -109,6 +147,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     {
         super.onActivityResult(requestCode, resultCode, data);
 
+        // Pass the activity result back to the Facebook SDK
+
+
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN)
         {
@@ -125,7 +166,12 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 // ...
             }
         }
+        else
+        {
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
+
 
     private void StoreData(FirebaseUser mUser)
     {
@@ -152,7 +198,38 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                             // If sign in fails, display a message to the user.
                             //Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toaster("Authentication failed.");
+                            mAuth.signOut();
                         }
+                    }
+                });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token)
+    {
+        //Log.d(TAG, "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            StoreData(user);
+                        }
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toaster("Firebase side - Authentication failed.");
+                            mAuth.signOut();
+                        }
+
+                        // ...
                     }
                 });
     }
@@ -215,7 +292,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 }
                 else if(buttonID == R.id.btnFb)
                 {
-
+                    //SignInFb();
                 }
                 else if(buttonID == R.id.btnGoogle)
                 {
